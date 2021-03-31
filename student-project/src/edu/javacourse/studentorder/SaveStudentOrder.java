56396888d71d8d7faa984e3533a178695f0df0fa
Pasha Shyla <pashashelo@gmail.com>
2021-03-31 17:56:48 +0300

    Add University class

--- student-project/src/edu/javacourse/studentorder/SaveStudentOrder.java
+++ student-project/src/edu/javacourse/studentorder/SaveStudentOrder.java
@@ -101,6 +101,8 @@ public class SaveStudentOrder
         husband.setIssueDepartment(po1);
         husband.setStudentId("" + (100000 + id));
         husband.setAddress(address);
+        husband.setUniversity(new University(2L, ""));
+        husband.setStudentId("HH12345");
 
         // Жена
         Adult wife = new Adult("Петрова", "Вероника", "Алекссевна", LocalDate.of(1998, 3, 12));
@@ -111,6 +113,8 @@ public class SaveStudentOrder
         wife.setIssueDepartment(po2);
         wife.setStudentId("" + (200000 + id));
         wife.setAddress(address);
+        wife.setUniversity(new University(1L, ""));
+        wife.setStudentId("WW12345");
 
         // Ребенок
         Child child1 = new Child("Петрова", "Ирина", "Викторовна", LocalDate.of(2018, 6, 29));
